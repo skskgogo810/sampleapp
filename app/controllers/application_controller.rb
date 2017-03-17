f6a30a79ba15a6abe816f0e1dac086a06814590c
skskgogo810 <mahu2315-saka0135@docomo.ne.jp>
2017-03-17 21:19:28 +0900

    initialize commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-      render html: "hello world sample"
+      render html: "hello world!"
   end
 end
